8ff84c131a9d66a65765585ea57626cb2156ad73
Cheng, YuChang | Dixen | AIPD <yuchang.cheng@rakuten.co.jp>
2018-05-16 18:29:04 +0900

    stash

--- src/main/java/idv/lc/array/MedianOfTwoArrays.java
+++ src/main/java/idv/lc/array/MedianOfTwoArrays.java
@@ -8,32 +8,38 @@ class MedianOfTwoArrays{
         int e2 = nums2.length - 1;
         int totalLen = e1 + e2 + 2;
         if(totalLen % 2 == 0) {
-            int r = helper(nums1, 0, e1, nums2, 0, e2, (totalLen)/2-1);
-            int l = helper(nums1, 0, e1, nums2, 0, e2, (totalLen)/2-2);
-            System.out.printf("l: %d, r:%d\n", l, r);
+            int r = helper(nums1, 0, e1, nums2, 0, e2, (totalLen)/2);
+            int l = helper(nums1, 0, e1, nums2, 0, e2, (totalLen)/2-1);
             return (l+r) / 2.0;
         } else {
-            return helper(nums1, 0, e1, nums2, 0, e2, (totalLen)/2) / 1.0;
+            return helper(nums1, 0, e1, nums2, 0, e2, (totalLen)/2-1) / 1.0;
         }
     }
     // find k th elemnet in two sorted array (k from 0)
-    private int helper(int[] n, int s1, int e1, int[] m, int s2, int e2, int k) {
-        System.out.printf("%d, %d, %d, %d, %d\n", s1, e1, s2, e2, k);
-        if(s1 == e1 && s1 != 0) return m[k];
-        if(s2 == e2 && s2 != 0) return n[k];
+    public int helper(int[] n, int s1, int e1, int[] m, int s2, int e2, int k) {
+        System.out.printf(">>>>>   %d, %d, %d, %d, %d\n", s1, e1, s2, e2, k);
+        // terminate condition
+        if(k == 0) return Math.min(n[s1], m[s2]);
+        if(s1 == e1) return m[k];
+        if(s2 == e2) return n[k];
+        // main part
         int mid1 = (s1 + e1) / 2;
         int mid2 = (s2 + e2) / 2;
-        if (mid1 + mid2 < k) { //取不夠, 要往s1 or s2 = mid1 or mid2 方向取
+        if (mid1+mid2 < k) { //取不夠, 要往s1 or s2 = mid1 or mid2 方向取
             if(n[mid1] > m[mid2]) {
-                return helper(n, s1, e1, m, mid2+1, e2, k-mid2-1);
+                System.out.println("a");
+                return helper(n, s1, e1, m, mid2+1, e2, k-(mid2+1-s2));
             } else {
-                return helper(n, mid1+1, e1, m, s1, e2, k-mid1-1);
+                System.out.println("b");
+                return helper(n, mid1+1, e1, m, s1, e2, k-(mid1+1-s1));
             }
         } else {
             if(n[mid1] > m[mid2]) {
-                return helper(n, s1, mid1, m, s2, e2, k-mid1);
+                System.out.println("c");
+                return helper(n, s1, mid1, m, s2, e2, k);
             } else {
-                return helper(n, s1, e1, m, s1, mid2, k-mid2);
+                System.out.println("d");
+                return helper(n, s1, e1, m, s2, mid2, k);
             }
         }
     }
--- src/test/java/idv/lc/array/MedianOfTwoArraysTest.java
+++ src/test/java/idv/lc/array/MedianOfTwoArraysTest.java
@@ -6,7 +6,31 @@ import java.util.*;
 
 public class MedianOfTwoArraysTest{
     MedianOfTwoArrays s = new MedianOfTwoArrays();
+    /*
     @Test
+    public void test0(){
+        int[] num1 = new int[]{1,2};
+        int[] num2 = new int[]{3,4};
+        assertEquals(1, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 0));
+        assertEquals(2, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 1));
+        assertEquals(3, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 2));
+        assertEquals(4, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 3));
+    }*/
+    @Test
+    public void test1(){
+        int[] num1 = new int[]{2,3,6,7,9};
+        int[] num2 = new int[]{1,4,8,10};
+        //assertEquals(1, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 0));
+        //assertEquals(2, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 1));
+        //assertEquals(3, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 2));
+        //assertEquals(4, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 3));
+        //assertEquals(6, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 4));
+        //assertEquals(7, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 5));
+        assertEquals(8, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 6));
+        //assertEquals(9, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 7));
+        //assertEquals(10, s.helper(num1, 0, num1.length-1, num2, 0, num2.length-1, 8));
+    }
+    /*
     public void test1(){
         int[] num1 = new int[]{1,3};
         int[] num2 = new int[]{2};
@@ -15,5 +39,5 @@ public class MedianOfTwoArraysTest{
         num2 = new int[]{3,4};
         assertEquals(2.5, s.findMedianSortedArrays(num1, num2), 0.0001);
 
-    }
+    }*/
 }
