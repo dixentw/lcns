83e505132a4ee8de2ab42d4dadee2d716be9fd17
Cheng, YuChang | Dixen | AIPD <yuchang.cheng@rakuten.com>
2018-05-31 12:45:04 +0900

    581. Shortest Unsorted Continuous Subarray

--- src/main/java/idv/lc/array/ShortestUnSort.java
+++ src/main/java/idv/lc/array/ShortestUnSort.java
@@ -6,30 +6,18 @@ class ShortestUnSort {
     public int findUnsortedSubarray(int[] nums) {
         int start = 0;
         int end = 0;
+        int n = nums.length;
+        int localMax = nums[0];
+        int localMin = nums[n-1];
         for(int i=1; i<nums.length; i++) {
-            if(nums[i] < nums[i-1]) {
-                if(start==0) {
-                    start = i;
-                }else{
-                    end = i;
-                }
-            }
+            localMax = Math.max(localMax, nums[i]);
+            localMin = Math.min(localMin, nums[n-i-1]);
+            if(nums[i] < localMax) end = i;
+            if(nums[n-1-i] > localMin) start = n-1-i;
         }
-        System.out.printf("%d, %d\n", start, end);
-        if(start==0 && end==0) {
+        if(start ==0 && end ==0){
             return 0;
         }
-        int localMax = Integer.MIN_VALUE;
-        for(int i=start-1;i<=end; i++) {
-            localMax = Math.max(localMax, nums[i]);
-        }
-        int realEnd = 0;
-        for(int i=end; i<nums.length; i++) {
-            if(localMax < nums[i]){
-                realEnd = i;
-                break;
-            }
-        }
-        return realEnd - start+1;
+        return end-start+1;
     }
 }
--- src/test/java/idv/lc/array/ShortestUnSortTest.java
+++ src/test/java/idv/lc/array/ShortestUnSortTest.java
@@ -8,11 +8,11 @@ public class ShortestUnSortTest {
     ShortestUnSort s = new ShortestUnSort();
     @Test
     public void test1(){
-        int[] arr = new int[]{2, 6, 4, 8, 10, 9, 15};
-        assertEquals(5, s.findUnsortedSubarray(arr));
-        arr = new int[]{1,2,3,4};
-        assertEquals(0, s.findUnsortedSubarray(arr));
-        arr = new int[]{2,1};
-        assertEquals(2, s.findUnsortedSubarray(arr));
+        assertEquals(5, s.findUnsortedSubarray(new int[]{2, 6, 4, 8, 10, 9, 15}));
+        assertEquals(0, s.findUnsortedSubarray(new int[]{1,2,3,4}));
+        assertEquals(2, s.findUnsortedSubarray(new int[]{2,1}));
+        assertEquals(2, s.findUnsortedSubarray(new int[]{1,3,2,4,5}));
+        assertEquals(3, s.findUnsortedSubarray(new int[]{1,2,4,5,3}));
+        assertEquals(3, s.findUnsortedSubarray(new int[]{2,3,3,2,3}));
     }
 }
