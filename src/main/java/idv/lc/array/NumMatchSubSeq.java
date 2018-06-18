e34ae468808c026e4f34d067bda653f5be152d28
Dixen Cheng <dixentw@gmail.com>
2018-06-18 22:26:15 +0900

    792. Number of Matching Subsequences revise

--- src/main/java/idv/lc/array/NumMatchSubSeq.java
+++ src/main/java/idv/lc/array/NumMatchSubSeq.java
@@ -4,6 +4,46 @@ import java.util.*;
 
 class NumMatchSubSeq {
     public int numMatchingSubseq(String S, String[] words) {
+        Map<Character, List<String>> table = new HashMap<>();
+        for(String w : words) {
+            List<String> strs = table.get(w.charAt(0));
+            if(strs==null){
+                List<String> tmp = new ArrayList<>();
+                tmp.add(w);
+                table.put(w.charAt(0), tmp);
+            }else{
+                strs.add(w);
+            }
+        }
+        for(char c : S.toCharArray()){
+            List<String> ww = table.get(c);
+            for(String word : ww){
+                word = word.substring(1);
+                if(word.length()==0){
+                    List<String> strs = table.get(' ');
+                    if(strs==null){
+                        List<String> tmp = new ArrayList<>();
+                        tmp.add("");
+                        table.put(' ', tmp);
+                    }else{
+                        strs.add("");
+                    }
+                    continue;
+                }
+                List<String> strs = table.get(word.charAt(0));
+                if(strs==null){
+                    List<String> tmp = new ArrayList<>();
+                    tmp.add(word);
+                    table.put(word.charAt(0), tmp);
+                }else{
+                    strs.add(word);
+                }
+            }
+        }
+        return table.get(' ').size();
+
+    }
+    public int numMatchingSubseq1(String S, String[] words) {
         Map<Character, List<Integer>> table = new HashMap<>();
         for(int i=0; i<S.length(); i++){
             List<Integer> l = table.get(S.charAt(i));
