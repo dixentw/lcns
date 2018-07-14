57643cf530fdeaf2b42206ffcdb4e88677f27107
Dixen Cheng <dixentw@gmail.com>
2018-07-14 13:52:58 +0900

    70. Climbing Stairs - revise

--- src/main/java/idv/lc/dp/ClimbingStairs.java
+++ src/main/java/idv/lc/dp/ClimbingStairs.java
@@ -8,13 +8,13 @@ public class ClimbingStairs {
         if (n==1) {
             return 1;
         }
-        int[] ways = new int[n+1];
-        ways[0] = 1;
-        ways[1] = 1;
-        for (int i=2; i<=n; i++) {
+        int[] ways = new int[n];
+        ways[0] = 1; // climb to first step take one way
+        ways[1] = 2; // climb to second step take two ways
+        for (int i=2; i<n; i++) {
             ways[i] = ways[i-1] + ways[i-2];
         }
-        return ways[n];
+        return ways[n-1];
 
     }
 }
