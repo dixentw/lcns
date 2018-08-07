497481b3e60cd050631ec3c4745ccc9b18d5945c
Dixen Cheng <dixentw@gmail.com>
2018-08-07 20:19:46 +0900

    62. Unique Paths - revisit

--- src/main/java/idv/lc/array/UniqlePath.java
+++ src/main/java/idv/lc/array/UniqlePath.java
@@ -7,6 +7,19 @@ public class UniqlePath {
         int[][] DP = new int[m][n];
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
+                if (i==0 || j==0) {
+                    DP[i][j] = 1;
+                } else {
+                    DP[i][j] = DP[i-1][j] + DP[i][j-1];
+                }
+            }
+        }
+        return DP[m-1][n-1];
+    }
+    public int uniquePaths_2(int m, int n) {
+        int[][] DP = new int[m][n];
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
                 if (i==0 && j==0) {
                     DP[i][j] = 1;
                 } else if (i == 0) {
