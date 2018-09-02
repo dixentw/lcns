871cad38ff1ee9e01f0178180c13eab6b6d9196e
Dixen Cheng <dixentw@gmail.com>
2018-09-02 17:10:53 +0900

    revise two sum

--- src/main/java/idv/lc/array/TwoSum.java
+++ src/main/java/idv/lc/array/TwoSum.java
@@ -4,6 +4,30 @@ import java.util.*;
 
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> table = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            if (table.containsKey(target - nums[i])) {
+                Integer idx = table.get(target - nums[i]);
+                return new int[]{Math.min(i, idx), Math.max(i, idx)};
+            }
+            table.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> table = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            table.put(nums[i], i);
+        }
+        for (int i=0; i<nums.length; i++) {
+            Integer pair = table.get(target - nums[i]);
+            if (pair != null && pair != i) {
+                return new int[]{Math.min(i, pair), Math.max(i, pair)};
+            }
+        }
+        return new int[]{};
+    }
+    public int[] twoSum1(int[] nums, int target) {
         int[] tmp = Arrays.copyOf(nums, nums.length);
         Arrays.sort(tmp);
         int i = 0;
