43f352e0eb86be81d1d71b154f39461c9353a0c0
Dixen Cheng <dixentw@gmail.com>
2018-09-15 20:39:32 +0900

    33. Search in Rotated Sorted Array

--- src/main/java/idv/lc/array/SearchRotate.java
+++ src/main/java/idv/lc/array/SearchRotate.java
@@ -3,6 +3,10 @@ package idv.lc.array;
 import java.util.*;
 
 class SearchRotate{
+    /*
+     * 依樣是binary search, 只是還要多判斷頭尾
+     * 判斷準則：確定好斷裂發生的地方，target有沒有發生在那個地方，所以要前後夾需要兩個條件&&
+     */
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length-1;
@@ -11,7 +15,7 @@ class SearchRotate{
             if(nums[mid] == target) {
                 return mid;
             }else if(nums[mid] > target) {
-                if(target <= nums[end] && nums[mid] > nums[end]) {
+                if(target <= nums[end] && nums[mid] > nums[end]) { //我要保證斷裂發生的地方在後面，而且target要小於end才會在斷裂裡面
                     start = mid + 1;
                 }else{
                     end = mid - 1;
