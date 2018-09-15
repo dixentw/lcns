83dd3b75f510201522235564f24d1eca83195b1c
Dixen Cheng <dixentw@gmail.com>
2018-09-15 16:06:30 +0900

    532. K-diff Pairs in an Array

--- src/main/java/idv/lc/array/KDiffPair.java
+++ src/main/java/idv/lc/array/KDiffPair.java
@@ -5,15 +5,39 @@ import java.util.*;
 class KDiffPair {
     /*
      * sort the array, find the k diff and return result
+     * 但是，不能只看往前一個，有可能要看到好幾個，基本邏輯必須應用Two-Sum
+     * Naive是O(n^2)解
+     * 我們有HashMap跟Two pointer解法
      */
+
     public int findPairs(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int cnt = 0;
+        for (int i=0, j=1; i<n&&j<n; j = Math.max(i+1, j)) { // 處理dup時會造成i ==j, 這時候需要強迫+1
+            if (nums[j] - nums[i] == k) {
+                cnt++;
+                while(j<n-1&&nums[j+1]==nums[j]) j++;
+                j++;
+                while(i<n-1&&nums[i+1]==nums[i]) i++;
+                i++;
+            } else if (nums[j] - nums[i] < k) {
+                while(j<n-1&&nums[j+1]==nums[j]) j++;
+                j++;
+            } else {
+                while(i<n-1&&nums[i+1]==nums[i]) i++;
+                i++;
+            }
+        }
+        return cnt;
+    }
+    public int findPairsOld(int[] nums, int k) {
         Arrays.sort(nums);
         int p = 0;
         for(int i = 0; i<nums.length-1; i++) {
             if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
-            System.out.println(i);
             for(int j=i+1; j<nums.length;j++){
                 if(nums[j]-nums[i]==k){
                     p++;
--- src/test/java/idv/lc/array/KDiffPairTest.java
+++ src/test/java/idv/lc/array/KDiffPairTest.java
@@ -10,6 +10,7 @@ public class KDiffPairTest {
     public void test1(){
         assertEquals(2, s.findPairs(new int[]{3,1,4,1,5}, 2));
         assertEquals(4, s.findPairs(new int[]{1,2,3,4,5}, 1));
+        assertEquals(1, s.findPairs(new int[]{3,1,4,1,5}, 0));
         assertEquals(1, s.findPairs(new int[]{1,1,1,1,1}, 0));
     }
 }
