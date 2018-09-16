91c619bb17d4ab9c28911d6cfc65b89488f72fc8
Dixen Cheng <dixentw@gmail.com>
2018-09-16 17:39:57 +0900

    581. Shortest Unsorted Continuous Subarray

--- src/main/java/idv/lc/array/ShortestUnSort.java
+++ src/main/java/idv/lc/array/ShortestUnSort.java
@@ -3,7 +3,27 @@ package idv.lc.array;
 import java.util.*;
 
 class ShortestUnSort {
+    /*
+     *  沒有sort過的array，就是從前面數來最大的那個element
+     *  跟後面數來最小的那個element的長度
+     */
     public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int max = nums[0];
+        int min =nums[n-1];
+        int maxIdx = 0;
+        int minIdx = 0;
+        for (int i=0; i<nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n-1-i]);
+            if (nums[i] < max) maxIdx = i;
+            if (nums[n-1-i] > min) minIdx = n-1-i;
+        }
+        if (maxIdx==0 && minIdx ==0) return 0;
+        return maxIdx - minIdx + 1;
+    }
+
+    public int findUnsortedSubarrayOld(int[] nums) {
         int start = 0;
         int end = 0;
         int n = nums.length;
--- src/test/java/idv/lc/array/ShortestUnSortTest.java
+++ src/test/java/idv/lc/array/ShortestUnSortTest.java
@@ -8,8 +8,8 @@ public class ShortestUnSortTest {
     ShortestUnSort s = new ShortestUnSort();
     @Test
     public void test1(){
-        assertEquals(5, s.findUnsortedSubarray(new int[]{2, 6, 4, 8, 10, 9, 15}));
         assertEquals(0, s.findUnsortedSubarray(new int[]{1,2,3,4}));
+        assertEquals(5, s.findUnsortedSubarray(new int[]{2, 6, 4, 8, 10, 9, 15}));
         assertEquals(2, s.findUnsortedSubarray(new int[]{2,1}));
         assertEquals(2, s.findUnsortedSubarray(new int[]{1,3,2,4,5}));
         assertEquals(3, s.findUnsortedSubarray(new int[]{1,2,4,5,3}));
