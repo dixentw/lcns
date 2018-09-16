567606c2452b68f2669a592c63bc1119926c396d
Dixen Cheng <dixentw@gmail.com>
2018-09-16 19:45:19 +0900

    448. Find All Numbers Disappeared in an Array

--- src/main/java/idv/lc/array/FindDisapearedNum.java
+++ src/main/java/idv/lc/array/FindDisapearedNum.java
@@ -3,6 +3,13 @@ package idv.lc.array;
 import java.util.*;
 
 class FindDisapearedNum {
+    /*
+     * 掃一遍，使用value as index把原本的數組變成負數
+     * >>> (第二次想沒想通) 如果已經是負數就不再標註負數
+     * 最後是不是負數的那幾個就是
+     *
+     * [FAIL] - nums[val]要減一 -> nums[val-1]
+     */
     public List<Integer> findDisappearedNumbers(int[] nums) {
         for(int i=0; i<nums.length; i++){
             int val = Math.abs(nums[i]);
