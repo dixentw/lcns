288cf0a00b9fff51d41910729cda5465e779eb3b
Dixen Cheng <dixentw@gmail.com>
2018-09-16 21:51:05 +0900

    219. Contains Duplicate II

--- src/main/java/idv/lc/array/ContainDups2.java
+++ src/main/java/idv/lc/array/ContainDups2.java
@@ -3,7 +3,11 @@ package idv.lc.array;
 import java.util.*;
 
 class ContainDups2 {
-    // naive, too slow
+    //
+    // 找到兩個元素，在k個範圍內是一樣的
+    //
+    // naive, too slow => 對每個i, 尋找 [i-k,i+k]的每個pair都做一遍，要注意i!=j
+    //
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         for(int i=0; i<nums.length; i++){
             for(int j=i-k; j<=i+k; j++){
@@ -14,20 +18,16 @@ class ContainDups2 {
         }
         return false;
     }
+    // better, 然後不用那麼煩麻，只要num.....num 的距離超過k, 就update 第二個num的index,
+    // 因為第三個num跟第一個一定大於 k, 所以判斷第三num跟第二num的距離就好了
     public boolean containsNearbyDuplicate2(int[] nums, int k) {
-        Map<Integer, Integer> table = new HashMap<Integer, Integer>();
-        for(int i=0; i<nums.length; i++){
-            Integer saved = table.get(nums[i]);
-            if(saved==null){
-                table.put(nums[i], i);
-            }else{
-                int diff = Math.abs(saved - i);
-                if(diff <= k){
-                    return true;
-                }else{
-                    table.put(nums[i], i);
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int diff = Math.abs(map.get(nums[i]) - i);
+                if (diff <= k) return true;
             }
+            map.put(nums[i], i);
         }
         return false;
     }
