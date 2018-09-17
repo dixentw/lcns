f804bef1343d26158248426fbee0b6e9d2583c64
Dixen Cheng <dixentw@gmail.com>
2018-09-17 21:59:23 +0900

    665. Non-decreasing Array

--- src/main/java/idv/lc/array/NonDecreaseArr.java
+++ src/main/java/idv/lc/array/NonDecreaseArr.java
@@ -3,6 +3,17 @@ package idv.lc.array;
 import java.util.*;
 
 class NonDecreaseArr {
+    /*
+     * 檢查decrese, 如果後面大於前面的decrese, 那就ok <- 錯誤假設
+     * 
+     * 如何判斷個數？還是要用cnt
+     * [FAIL]沒有判斷 array length==1
+     * [FAIL]錯誤假設
+     * 使用線圖判斷不完判斷山谷很麻煩
+     * 為了解決這個麻煩，我們可以直接先修改array的內容，以i的值為準
+     * 改了之後再去看count有沒有合標
+     *
+     */
     public boolean checkPossibility(int[] nums) {
         int count = 0;
         int smallIdx = 0;
