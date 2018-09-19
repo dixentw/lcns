935b8f29855ed3e17bbb293763e433f4b56c9c6b
Dixen Cheng <dixentw@gmail.com>
2018-09-19 19:53:29 +0900

    681. Next Closest Time

--- src/main/java/idv/lc/string/NextClosestTime.java
+++ src/main/java/idv/lc/string/NextClosestTime.java
@@ -8,7 +8,6 @@ public class NextClosestTime {
         int m = Integer.parseInt(time.substring(3));
         for (int i = 60 * h + m+1; ; i++) {
             String s = String.format("%02d:%02d", i/60%24, i%60);
-            System.out.println(s);
             if (s.matches("[" +time+ "]+")) {
                 return s;
             }
