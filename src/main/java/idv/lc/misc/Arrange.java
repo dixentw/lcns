67c8738f3d2eafd491ce12e8ed3739507ebe2ae1
Cheng, YuChang | Dixen | AIPD <yuchang.cheng@rakuten.com>
2018-09-27 15:42:39 +0900

    misc

--- src/main/java/idv/lc/misc/Arrange.java
+++ src/main/java/idv/lc/misc/Arrange.java
@@ -10,27 +10,34 @@ public class Arrange {
             for (int j=0; j<n; j++) {
                 int total = 4;
                 if (i-1 < 0) total--;
-                if (i+1 > m) total--;
+                if (i+1 >= m) total--;
                 if (j-1 < 0) total--;
-                if (j+1 > n) total--;
+                if (j+1 >= n) total--;
                 seats[i][j] = total;
                 so.add(new Integer[]{total, i, j});
             }
         }
-        Collections.sort(so, (Integer[] a, Integer[] b) -> b[0] - a[0]);
         for (int i=0; i<m*n-people; i++) { //要拿掉的部分
-            Integer[] data = so.get(i);
-            int x = data[1];
-            int y = data[2];
-            seats[x][y] = 0;
-            if (x-1 >=0) seats[x-1][y]--;
-            if (x+1 < m) seats[x+1][y]--;
-            if (y-1 >=0) seats[x][y-1]--;
-            if (y+1 < n) seats[x][y+1]--;
+            int max = 0;
+            int mX = 0;
+            int mY = 0;
+            for (int j=0; j<m; j++) {
+                for (int k=0; k<n; k++) {
+                    if (seats[j][k] > max) {
+                        max = seats[j][k];
+                        mX = j;
+                        mY = k;
+                    }
+                }
+            }
+            seats[mX][mY] = 0;
+            if (mX-1 >=0 && seats[mX-1][mY]>0) seats[mX-1][mY]--;
+            if (mX+1 < m && seats[mX+1][mY]>0) seats[mX+1][mY]--;
+            if (mY-1 >=0 && seats[mX][mY-1]>0) seats[mX][mY-1]--;
+            if (mY+1 < n && seats[mX][mY+1]>0) seats[mX][mY+1]--;
         }
         int sum = 0;
         for (int i=0; i<m; i++) {
-            System.out.println(Arrays.toString(seats[i]));
             for (int j=0; j<n; j++) {
                 sum += seats[i][j];
             }
--- src/test/java/idv/lc/misc/ArrangeTest.java
+++ src/test/java/idv/lc/misc/ArrangeTest.java
@@ -9,5 +9,15 @@ public class ArrangeTest {
     @Test
     public void test1(){
         assertEquals(7, s.getMinArrange(5, 2, 8));
+        assertEquals(18, s.getMinArrange(3, 5, 14));
+        assertEquals(0, s.getMinArrange(1, 16, 1));
+        assertEquals(10, s.getMinArrange(8, 2, 12));
+        assertEquals(0, s.getMinArrange(15, 1, 6));
+
+        assertEquals(18, s.getMinArrange(5,3,14));
+        assertEquals(2, s.getMinArrange(4,3,7));
+        assertEquals(0, s.getMinArrange(6,2,4));
+        assertEquals(0, s.getMinArrange(3,5,1));
+        assertEquals(16, s.getMinArrange(6,2,12));
     }
 }
