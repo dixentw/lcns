048cf76a8ad5b740bf6545a9e4ad497271eb4837
Dixen Cheng <dixentw@gmail.com>
2018-10-03 21:10:39 +0900

    95. Unique Binary Search Trees II

---
+++ src/main/java/idv/lc/tree/LC95.java
@@ -0,0 +1,39 @@
+package idv.lc.tree;
+
+import java.util.*;
+
+public class LC95 {
+    public List<TreeNode> generateTrees(int n) {
+        if (n<=0) return new List<TreeNode>(); //leetcode wierd init test case
+        return helper(1, n);
+    }
+    public List<TreeNode> helper(int min, int max) {
+        List<TreeNode> res = new ArrayList<>();
+        if (min > max) {
+            list.add(null);
+            return list;
+        }
+        /* by calling helper() for left child and right child
+         * both will return null
+         * so append it with inner most loop is sufficient for this block
+        if (min == max) {
+            list.add(new TreeNode(min));
+            return list;
+        }*/
+        List<TreeNode> left;
+        List<TreeNode> right;
+        for (int i=min, i<=max; i++) {
+            left = helper(min, i-1);
+            right = helper(i+1, max);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
---
+++ src/test/java/idv/lc/tree/LC95Test.java
@@ -0,0 +1,12 @@
+package idv.lc.tree;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class LC95Test {
+    LC95 s = new LC95();
+    @Test
+    public void test1(){
+    }
+}
