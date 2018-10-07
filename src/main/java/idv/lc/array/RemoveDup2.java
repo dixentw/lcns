8d9733617bc751479860561a6a06eac0f5888578
Dixen Cheng <dixentw@gmail.com>
2018-10-07 11:29:24 +0900

    80. Remove Duplicates from Sorted Array II

--- src/main/java/idv/lc/array/RemoveDup2.java
+++ src/main/java/idv/lc/array/RemoveDup2.java
@@ -4,6 +4,16 @@ import java.util.*;
 
 class RemoveDup2 {
     public int removeDuplicates(int[] nums) {
+        int start = 2;
+        for(int i=2; i<nums.length;i++){
+            if (nums[i] != nums[start-2]) {
+                nums[start++] = nums[i];
+            }
+        }
+        return start;
+    }
+
+    public int removeDuplicatesOLD(int[] nums) {
         int start = 0;
         int cnt = 0;
         for(int i=1; i<nums.length;i++){
