8bc1b28473e09881b70b2934d9b1e270e32502f4
Dixen Cheng <dixentw@gmail.com>
2018-10-08 19:01:15 +0900

    209. Minimum Size Subarray Sum

--- src/main/java/idv/lc/array/MinSizeSubArr.java
+++ src/main/java/idv/lc/array/MinSizeSubArr.java
@@ -4,6 +4,24 @@ import java.util.*;
 
 class MinSizeSubArr {
     public int minSubArrayLen(int s, int[] nums) {
+        if (nums.length == 0) return 0;
+        int start = 0;
+        int end = 0;
+        int sum = nums[0];
+        int mLen = Integer.MAX_VALUE; // wrong init with 0
+        while (start < nums.length && end < nums.length) {
+            if (sum < s) {
+                end++;
+                if (end == nums.length) continue;
+                sum += nums[end];
+            } else {
+                mLen = Math.min(mLen, end-start+1);
+                sum -= nums[start++];
+            }
+        }
+        return (mLen == Integer.MAX_VALUE) ? 0 : mLen;
+    }
+    public int minSubArrayLenOld(int s, int[] nums) {
         int start = 0;
         int end = 0;
         int length = Integer.MAX_VALUE;
