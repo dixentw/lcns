7c5020534ec1ca8b7d401d0c93e3bc148615aea4
Dixen Cheng <dixentw@gmail.com>
2018-10-30 18:56:37 +0900

    894. All Possible Full Binary Trees

---
+++ src/test/java/idv/lc/tree/LC894Test.java
@@ -0,0 +1,12 @@
+package idv.lc.tree;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class LC894Test {
+    LC894 s = new LC894();
+    @Test
+    public void test1(){
+    }
+}
