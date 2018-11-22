c71257b7dd683d158c21c6ef3fb7bccd07cbc166
Dixen Cheng <dixentw@gmail.com>
2018-11-22 19:20:02 +0900

    672. Bulb Switcher II

---
+++ src/main/java/idv/lc/math/LC672.java
@@ -0,0 +1,15 @@
+package idv.lc.math;
+
+import java.util.*;
+
+public class LC672 {
+    public int flipLights(int n, int m) {
+        if (m == 0) return 1;
+        if (n==1) return 2;
+        if (n==2 && m==1) return 3;
+        if (n==2 && m>=2) return 4;
+        if (m==1) return 4;
+        if (m==2) return 7;
+        if (m==3) return 8;
+    }
+}
---
+++ src/test/java/idv/lc/math/LC672Test.java
@@ -0,0 +1,12 @@
+package idv.lc.math;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class LC672Test {
+    LC672 s = new LC672();
+    @Test
+    public void test1(){
+    }
+}
