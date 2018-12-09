487009ce5943c67382e692e3b5f7b661dd623c53
Dixen Cheng <dixentw@gmail.com>
2018-12-09 22:13:14 +0900

    426. Convert Binary Search Tree to Sorted Doubly Linked List

--- src/main/java/idv/lc/tree/LC426.java
+++ src/main/java/idv/lc/tree/LC426.java
@@ -3,7 +3,26 @@ package idv.lc.tree;
 import java.util.*;
 
 public class LC426 {
+    TreeNode prev = null;
     public TreeNode treeToDoublyList(TreeNode root) {
+        if (root == null) return root;
+        TreeNode dummy = new TreeNode(0);
+        prev = dummy;
+        helper(root);
+        prev.right = dummy.right;
+        dummy.right.left = prev;
+        return dummy.right;
+    }
+    public void helper(TreeNode root) {
+        if (root == null) return;
+        helper(root.left);
+        prev.right = root;
+        root.left = prev;
+        prev = root;
+        helper(prev.right);
+    }
+
+    public TreeNode _treeToDoublyList(TreeNode root) {
         Stack<TreeNode> st = new Stack<>();
         TreeNode curr = root;
         TreeNode prev = null;
