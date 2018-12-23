7b900847c7543eea3a5b9f73f68ee48ddfc82e3f
Dixen Cheng <dixentw@gmail.com>
2018-12-23 16:51:06 +0900

    560. Subarray Sum Equals K

--- src/test/java/idv/lc/array/SubArrSumKTest.java
+++ src/test/java/idv/lc/array/SubArrSumKTest.java
@@ -8,9 +8,10 @@ public class SubArrSumKTest {
     SubArrSumK s = new SubArrSumK();
     @Test
     public void test1(){
-       // assertEquals(0, s.subarraySum(new int[]{1}, 0));
+        assertEquals(0, s.subarraySum(new int[]{1}, 0));
         assertEquals(0, s.subarraySum(new int[]{1, 1, 1}, 0));
-        //assertEquals(2, s.subarraySum(new int[]{1, 1, 1}, 2));
-        //assertEquals(1, s.subarraySum(new int[]{-1, -1, 1}, 0));
+        assertEquals(2, s.subarraySum(new int[]{1, 1, 1}, 2));
+        assertEquals(1, s.subarraySum(new int[]{-1, -1, 1}, 0));
+        assertEquals(7, s.subarraySum(new int[]{-3, 7, -7, 7, -7, 0, 7}, 7));
     }
 }
