7fdacc37bf2dee5d9558a00d9f0a88faea69c51f
Dixen Cheng <dixentw@gmail.com>
2019-01-12 11:37:32 +0900

    314. Binary Tree Vertical Order Traversal

--- src/main/java/idv/lc/tree/LC314.java
+++ src/main/java/idv/lc/tree/LC314.java
@@ -3,6 +3,31 @@ package idv.lc.tree;
 import java.util.*;
 
 public class LC314 {
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        Queue<Integer> col = new LinkedList<>();
+        Map<Integer, List<Integer>> map = new TreeMap<>();
+        q.offer(root);
+        col.offer(0);
+        while (!q.isEmpty()) {
+            TreeNode n = q.poll();
+            Integer c = col.poll();
+            List<Integer> l = map.getOrDefault(c, new ArrayList<Integer>());
+            l.add(n.val);
+            map.put(c, l);
+            if (n.left != null) {
+                q.offer(n.left);
+                col.offer(c-1);
+            }
+            if (n.right != null) {
+                q.offer(n.right);
+                col.offer(c+1);
+            }
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        for (Integer k : map.keySet()) res.add(map.get(k));
+        return res;
+    }
     class Data {
         int val;
         int level;
@@ -11,7 +36,7 @@ public class LC314 {
             this.level = l;
         }
     }
-    public List<List<Integer>> verticalOrder(TreeNode root) {
+    public List<List<Integer>> _verticalOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
         Map<Integer, List<Data>> table = new TreeMap<>();
