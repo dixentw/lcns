611f985f03c02ccba870052f001b0bfcfbca8cac
Dixen Cheng <dixentw@gmail.com>
2019-02-10 20:29:56 +0900

    297. Serialize and Deserialize Binary Tree

--- src/main/java/idv/lc/tree/LC297.java
+++ src/main/java/idv/lc/tree/LC297.java
@@ -4,8 +4,32 @@ import java.util.*;
 
 public class LC297 {
 
-    // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        List<String> res = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int n = q.size();
+            int nc = 0;
+            for (int i=0; i<n; i++) {
+                if(q.get(i)==null) nc++;
+            }
+            if (nc==n) break;
+            for (int i=0; i<n; i++) {
+                TreeNode node = q.poll();
+                if (node==null) {
+                    res.add("null");
+                } else {
+                    res.add(String.valueOf(node.val));
+                    q.offer(node.left);
+                    q.offer(node.right);
+                }
+            }
+        }
+        return "[" + String.join(",", res) + "]";
+    }
+    // Encodes a tree to a single string.
+    public String _serialize(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) return Arrays.toString(res.toArray());
         LinkedList<TreeNode> q = new LinkedList<>();
@@ -31,8 +55,30 @@ public class LC297 {
         return Arrays.toString(res.toArray()).replace(" ","");
     }
 
-    // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        //process data
+        if (data == "" || data.length()==0) return null;
+        String[] elems = data.substring(1, data.length()-1).split(",");
+        if (elems.length==0) return null;
+        if (elems[0].equals("null")) return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode root = new TreeNode(Integer.parseInt(elems[0]));
+        q.offer(root);
+        for (int i=1; i<elems.length; i+=2) {
+            TreeNode p = q.poll();
+            if (!elems[i].equals("null")) {
+                p.left = new TreeNode(Integer.parseInt(elems[i]));
+                q.offer(p.left);
+            }
+            if (!elems[i+1].equals("null")) {
+                p.right = new TreeNode(Integer.parseInt(elems[i+1]));
+                q.offer(p.right);
+            }
+        }
+        return root;
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode _deserialize(String data) {
         if (data == "") return null;
         String[] ns = data.substring(1, data.length()-1).split(",");
         ns = Arrays.stream(ns).filter(s -> !s.isEmpty()).toArray(String[]::new);
